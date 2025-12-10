affdcb85d9e479cb6800e441bbfef0e9a12993da
riyathakur11 <riya@code4each.com>
2025-12-10 11:24:30 +0530

    new migration and check domain code changes

---
+++ database/migrations/2025_12_10_051303_add_dns_verification_columns_to_domains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            $table->boolean('is_dns_matched')->default(false);
+            $table->timestamp('verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            $table->dropColumn(['is_dns_matched', 'verified_at']);
+        });
+    }
+};
